e5020520b79b1d92fea9cc5cf4d3a4b0012a0e5c
Purnama-Mi5B <purnamabebo26@gmail.com>
2020-01-15 09:39:35 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/aplikasilistnegara/MainActivity.java
@@ -0,0 +1,53 @@
+package id.ac.poliban.mi.aplikasilistnegara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<String> countries = new ArrayList<>();
+    {
+        countries.add("Indonesia");
+        countries.add("Malaysia");
+        countries.add("Singapore");
+        countries.add("Italia");
+        countries.add("Inggris");
+        countries.add("Belanda");
+        countries.add("Argentina");
+        countries.add("Chile");
+        countries.add("Mesir");
+        countries.add("Uganda");
+        countries.add("Palestina");
+        countries.add("India");
+        countries.add("Hongkong");
+        countries.add("Taiwan");
+        countries.add("Thailand");
+        countries.add("Bangladesh");
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar()!=null)
+            setTitle("APLIKASI LIST COUNTRIES");
+
+        ListView listView = findViewById(R.id.list_View);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.item_rows_negara, R.id.tv_item_negara, countries);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(((parent, view, position, id) -> {
+            Toast.makeText(this, "Anda Memilih "+countries.get(position), Toast.LENGTH_SHORT).show();
+        }));
+    }
+}
